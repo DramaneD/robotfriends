39c1e29cec39a715e06800987fa56b854d06f97e
DramaneD <ddramane63@gmail.com>
2018-10-26 21:53:21 +0000

    Starting the robotfriends with creating card component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
+import Card from './Card';
+import 'tachyons';
+import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Card id={robots[0].id} name={robots[0].name} emai={robots[0].email} />
+		<Card id={robots[1].id} name={robots[1].name} emai={robots[1].email} />
+		<Card id={robots[2].id} name={robots[2].name} emai={robots[2].email} />
+	</div>
+	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
